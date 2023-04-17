1946b760da7be140418c11fa587a3fc1a28bfbf7
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-04-17 18:07:59 -0300

    feat: localhost experience
    Added an express server to run the project locally and have the same
    experience that happens in github pages.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.argv[2] || 3050
+const app = express()
+
+app.use('/js', express.static(path.resolve(__dirname, 'js')))
+app.use('/css', express.static(path.resolve(__dirname, 'css')))
+app.use('/css-collection/pages', express.static(path.resolve(__dirname, 'pages')))
+
+app.get('/css-collection/*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`)
+})
