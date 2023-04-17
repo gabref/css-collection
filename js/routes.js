e74403e2a504e6f14814a49d809b07eba9f06b96
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-04-17 15:14:53 -0300

    dev: testing custom routing: routes base url

--- js/routes.js
+++ js/routes.js
@@ -1,16 +1,17 @@
+const baseUrl = '/css-collection/pages/'
 const routes = new Map([
     [404, {
-        htmlPath: '/pages/error/index.html',
+        htmlPath: baseUrl + 'error/index.html',
         title: '404 | Css Collection',
         description: 'Error Page'
     }],
     ['/', {
-        htmlPath: '/pages/home/index.html',
+        htmlPath: baseUrl + 'home/index.html',
         title: 'Home | Css Collection',
         description: 'Home Page'
     }],
     ['/about', {
-        htmlPath: '/pages/about/index.html',
+        htmlPath: baseUrl + 'about/index.html',
         title: 'About | Css Collection',
         description: 'About Page'
     }],
